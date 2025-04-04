1f4ae818dc8b5e70c91790a3e5e89512484496ab
Leoberta <leocalcio01@gmail.com>
2025-04-04 20:00:49 +0200

    Set up of conda virtual env with pytest

---
+++ prova.py
@@ -0,0 +1,17 @@
+
+############ Test File for Programming course ######################à
+
+########## git init of the repo with this void file ###########
+
+########## Adding tests for practice ################
+def inc(x):
+    return x + 1
+
+def test_answer_1():
+    assert inc(3) == 5
+    
+def test_answer_2():
+    assert inc(7) == 7
+
+
+    
